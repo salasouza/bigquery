6490caad23c0679d4e5aaaa88f51ddbc7bb9a99b
Salatiel Souza <sal.souzasa@gmail.com>
2021-09-10 15:25:55 -0300

    fix

--- timeonpage.sql
+++ timeonpage.sql
@@ -1,9 +1,17 @@
 SELECT
   date,
   fullVisitorId,
+  index,
+  value,
   visitNumber,
   hostname,
   pagePath,
+  pageviews,
+  eventCategory,
+  eventAction,
+  eventLabel,
+  region,
+  city,
   hitNumber,
   time,
   nextTime,
@@ -12,22 +20,38 @@ FROM(
   SELECT
     date,
     fullVisitorId,
+    customDimension.index,
+    customDimension.value,
     visitNumber,
     page.hostname,
     page.pagePath,
+    totals.pageviews,
+    eventInfo.eventCategory,
+    eventInfo.eventAction,
+    eventInfo.eventLabel,
+    geoNetwork.region,
+    geoNetwork.city,
     hitNumber,
     time,
     LEAD(hit.time, 1) OVER (PARTITION BY fullVisitorId, visitNumber ORDER BY hit.time ASC) as nextTime
-FROM `GOOGLE*`
+FROM `Google_2021*`
 CROSS JOIN UNNEST(hits) AS hit, UNNEST(hit.customDimensions) AS customDimension
 WHERE page.hostname LIKE "url" AND customDimension.index = 3 AND
-date BETWEEN '20210816' AND '20210820'AND hit.type = "PAGE"
+date BETWEEN '20210901' AND '20210910'
 )
 GROUP BY  date,
   fullVisitorId,
+  index,
+  value,
   visitNumber,
   hostname,
   pagePath,
+  pageviews,
+  eventCategory,
+  eventAction,
+  eventLabel,
+  region,
+  city,
   hitNumber,
   time,
   nextTime
